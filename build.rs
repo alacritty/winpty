7ea7fb6c5164b669e7bebf1bfe2b53317c73d4ca
Zac Pullar-Strecker <zacmps@gmail.com>
2017-12-25 18:56:03 +1300

    Added rust bindings

---
+++ build.rs
@@ -0,0 +1,58 @@
+extern crate bindgen;
+extern crate cc;
+
+use std::env;
+use std::path::PathBuf;
+use std::process::Command;
+
+fn main() {
+    // Generate version header
+    Command::new("cmd")
+        .args(&["/C", "./src/shared/UpdateGenVersion.bat"])
+        .current_dir("./src/shared")
+        .output()
+        .expect("Failed to generate GenVersion.h");
+
+    // Build winpty, only MSVC is supported
+    // 32bit *should* work but hasn't been tested
+    cc::Build::new()
+        .cpp(true)
+        .include("src/libwinpty")
+        .include("src/include")
+        .include("src/shared")
+        .include("src/gen")
+        .file("src/libwinpty/AgentLocation.cc")
+        .file("src/libwinpty/winpty.cc")
+        .file("src/shared/BackgroundDesktop.cc")
+        .file("src/shared/Buffer.cc")
+        .file("src/shared/DebugClient.cc")
+        .file("src/shared/GenRandom.cc")
+        .file("src/shared/OwnedHandle.cc")
+        .file("src/shared/StringUtil.cc")
+        .file("src/shared/WindowsSecurity.cc")
+        .file("src/shared/WindowsVersion.cc")
+        .file("src/shared/WinptyAssert.cc")
+        .file("src/shared/WinptyException.cc")
+        .file("src/shared/WinptyVersion.cc")
+        .define("COMPILING_WINPTY_DLL", None)
+        .flag("/EHsc") // Exception handling
+        .compile("winpty");
+
+    // Generate bindings
+    let bindings = bindgen::Builder::default()
+        .header("src/include/winpty.h")
+        .clang_arg("-x")
+        .clang_arg("c++")
+        // This breaks the bindings at the moment
+        .rustfmt_bindings(false)
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+
+    println!("cargo:rerun-if-changed=build.rs");
+}
